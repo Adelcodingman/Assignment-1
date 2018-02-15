2f7ce5b3891f8b138f60ec1a39f4e38160e71d34
Adelcodingman <36492769+Adelcodingman@users.noreply.github.com>
2018-02-15 02:10:13 +0200

    Add files via upload

---
+++ game-8.py
@@ -0,0 +1,22 @@
+z=100
+x=0
+y=0
+print ("total:",z)
+while true:
+    x=int(input("player 1 choose your mun :")
+        while (x<0 or x==0 or (yn-x)<0 or (x>(2*y)and (y!=0 or x==z))):
+          x=int(input())
+          z=z-x
+          print("total:",z)
+          if z==0
+              print ("player 1 wins ")
+          break
+          y=int(input("player  choose your num:")
+                while y<0 or y==0 or (z-x)<0 or y>(2*x):
+                y=int(input())
+                z=z-y
+                print ("total:",z)
+                if z==0
+                    print ("player 2 wins ")
+                break
+                
